23d1001808db3f8a432bb0b685f3a8a945ba064e
huangxulin <huangxulin@dmhxm.com>
2021-07-18 15:17:27 +0800

    使用 acme.sh 申请 SSL 证书

--- site/sidebar.js
+++ site/sidebar.js
@@ -1,6 +1,7 @@
 module.exports = {
   '/_post': [
     '_posts/',
+    '_posts/2021-07-17-use-acme-sh-apply-ssl-certificate',
     '_posts/2021-07-15-mysql-procedure-samples',
     '_posts/2021-07-04-java8-date-time-processing',
     '_posts/2021-05-12-vmware-centos7-boot-optimization',
